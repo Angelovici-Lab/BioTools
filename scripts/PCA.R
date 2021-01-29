ad7b4818e93091ae79652c4af02d3ad783529b46
Yen On Chan <yenon118@gmail.com>
2021-01-29 15:04:29 -0600

    Edited PCA scripts

--- scripts/PCA.R
+++ scripts/PCA.R
@@ -12,6 +12,7 @@ library(tibble)
 library(ggplot2)
 library(argparse)
 library(GGally)
+library(ggfortify)
 
 # Set seed
 set.seed(1)
@@ -21,7 +22,7 @@ set.seed(1)
 ## Input and output (Double check or make changes)
 #######################################################################
 # Specify input file (csv or tab delimited text file)
-input <- file.path("/home/ycth8/data/projects/HAPPI.GWAS/raw_data/Arabidopsis_1001/03_22_2020_Arabidopsis_1001_BAA.csv")
+input <- file.path("/data/users/ycth8/projects/BioTools/data/03_22_2020_Arabidopsis_1001_BAA.csv")
 
 # Specify an output folder so that all the results can be stored into the folder
 output <- file.path("/home/ycth8/data/projects/BioTools/output/07_25_2020")
@@ -67,13 +68,19 @@ print(head(df))
 ##
 #######################################################################
 
+#######################################################################
+## PCA do not allow NA, -Inf, and Inf
+#######################################################################
 # Drop all NAs
 df <- df %>% drop_na() %>% as.data.frame(stringsAsFactors=FALSE)
 
 # Drop rows with infinite sum
 df <- df[is.finite(rowSums(df[, start_column:ncol(df)])),]
 
-# Plot sactter plot matrix
+
+#######################################################################
+## Plot sactter plot matrix
+#######################################################################
 cat("\n Save image... \n")
 p <- ggscatmat(df, columns = start_column:ncol(df))
 ggsave(
@@ -87,23 +94,46 @@ ggsave(
 )
 cat("\n")
 
-#Performs a principal components analysis
-pca_info <- prcomp(df[, start_column:ncol(df)], center = TRUE, scale. = TRUE)
 
-# Get the standard deviations of the principal components
-sdev <- data.frame(
-  Trait = colnames(df)[start_column:ncol(df)],
-  sdev = pca_info$sdev,
-  stringsAsFactors = FALSE
-)
+#######################################################################
+## Organize data for PCA
+#######################################################################
+pca_df <- df[, c(1, start_column:ncol(df))]
 
-# Get the matrix of variable loadings
-rotation <- as.data.frame(pca_info$rotation, stringsAsFactors = FALSE)
-rotation <- tibble::rownames_to_column(rotation, var = "Trait")
+rownames(pca_df) <- pca_df[,1]
+pca_df <- pca_df[,-1]
 
-# Plot PC1 vs PC2
-p <- ggplot(rotation, aes(x=PC1, y=PC2)) +
-  geom_text(label=rotation$Trait)
+pca_df <- t(pca_df) %>%
+  as.data.frame(stringsAsFactors=TRUE)
+
+cat(rep("\n", 3))
+if(ncol(pca_df)>10){
+  print(head(pca_df[,1:10]))
+} else{
+  print(head(pca_df))
+}
+
+
+#######################################################################
+## Performs a principal components analysis
+#######################################################################
+pca <- prcomp(pca_df, center = TRUE, scale. = TRUE)
+
+pca_variance <- pca$sdev^2
+pca_variance_percentage <- round(pca_variance/sum(pca_variance)*100, digits=2)
+
+x <- pca$x %>% as.data.frame(stringsAsFactors=TRUE)
+
+
+#######################################################################
+## Plot PC1 vs PC2 with GGplot2
+#######################################################################
+p <- ggplot(x, aes(x=PC1, y=PC2)) +
+  geom_text(label=rownames(x)) +
+  labs(
+    x=paste0("PC1 - ", pca_variance_percentage[1], "%"),
+    y=paste0("PC2 - ", pca_variance_percentage[2], "%")
+  )
 
 ggsave(
   filename="PCA_Plot.jpg",
@@ -111,20 +141,33 @@ ggsave(
   path=output
 )
 
-# Get center
-center <- as.data.frame(pca_info$center, stringsAsFactors = FALSE)
-center <- tibble::rownames_to_column(center, var = "Trait")
 
-# Get scale
-scale <- as.data.frame(pca_info$scale, stringsAsFactors = FALSE)
-scale <- tibble::rownames_to_column(scale, var = "Trait")
+#######################################################################
+## Plot PC1 vs PC2 with AutoPlot
+#######################################################################
+p <- autoplot(pca, data=pca_df, label=TRUE)
+
+ggsave(
+  filename="PCA_Autolot.jpg",
+  plot=p,
+  path=output
+)
+
+
+#######################################################################
+## Get all PCA information
+#######################################################################
+sdev <- pca$sdev %>% as.data.frame(stringsAsFactors = FALSE)
+rotation <- pca$rotation %>% as.data.frame(stringsAsFactors = FALSE)
+center <- pca$center %>% as.data.frame(stringsAsFactors = FALSE)
+scale <- pca$scale %>% as.data.frame(stringsAsFactors = FALSE)
 
 # Get PC
-PC <- data.frame(pc1 = pca_info$x[, 1], pc2 = pca_info$x[, 2], stringsAsFactors = FALSE)
+PC <- pca$x %>% as.data.frame(stringsAsFactors = FALSE)
 
-# Save all outputs to files
+# # Save all outputs to files
 write.csv(sdev, file.path(output, "sdev.csv"), row.names = FALSE)
-write.csv(rotation, file.path(output, "rotation.csv"), row.names = FALSE)
+write.csv(rotation, file.path(output, "rotation.csv"), row.names = TRUE)
 write.csv(center, file.path(output, "center.csv"), row.names = FALSE)
 write.csv(scale, file.path(output, "scale.csv"), row.names = FALSE)
-write.csv(PC, file.path(output, "PC.csv"), row.names = FALSE)
+write.csv(PC, file.path(output, "PC.csv"), row.names = TRUE)
--- tools/PCA.R
+++ tools/PCA.R
@@ -12,6 +12,7 @@ library(tibble)
 library(ggplot2)
 library(argparse)
 library(GGally)
+library(ggfortify)
 
 # Set seed
 set.seed(1)
@@ -71,13 +72,19 @@ print(head(df))
 ##
 #######################################################################
 
+#######################################################################
+## PCA do not allow NA, -Inf, and Inf
+#######################################################################
 # Drop all NAs
 df <- df %>% drop_na() %>% as.data.frame(stringsAsFactors=FALSE)
 
 # Drop rows with infinite sum
 df <- df[is.finite(rowSums(df[, start_column:ncol(df)])),]
 
-# Plot sactter plot matrix
+
+#######################################################################
+## Plot sactter plot matrix
+#######################################################################
 cat("\n Save image... \n")
 p <- ggscatmat(df, columns = start_column:ncol(df))
 ggsave(
@@ -91,23 +98,46 @@ ggsave(
 )
 cat("\n")
 
-#Performs a principal components analysis
-pca_info <- prcomp(df[, start_column:ncol(df)], center = TRUE, scale. = TRUE)
 
-# Get the standard deviations of the principal components
-sdev <- data.frame(
-  Trait = colnames(df)[start_column:ncol(df)],
-  sdev = pca_info$sdev,
-  stringsAsFactors = FALSE
-)
+#######################################################################
+## Organize data for PCA
+#######################################################################
+pca_df <- df[, c(1, start_column:ncol(df))]
 
-# Get the matrix of variable loadings
-rotation <- as.data.frame(pca_info$rotation, stringsAsFactors = FALSE)
-rotation <- tibble::rownames_to_column(rotation, var = "Trait")
+rownames(pca_df) <- pca_df[,1]
+pca_df <- pca_df[,-1]
 
-# Plot PC1 vs PC2
-p <- ggplot(rotation, aes(x=PC1, y=PC2)) +
-  geom_text(label=rotation$Trait)
+pca_df <- t(pca_df) %>%
+  as.data.frame(stringsAsFactors=TRUE)
+
+cat(rep("\n", 3))
+if(ncol(pca_df)>10){
+  print(head(pca_df[,1:10]))
+} else{
+  print(head(pca_df))
+}
+
+
+#######################################################################
+## Performs a principal components analysis
+#######################################################################
+pca <- prcomp(pca_df, center = TRUE, scale. = TRUE)
+
+pca_variance <- pca$sdev^2
+pca_variance_percentage <- round(pca_variance/sum(pca_variance)*100, digits=2)
+
+x <- pca$x %>% as.data.frame(stringsAsFactors=TRUE)
+
+
+#######################################################################
+## Plot PC1 vs PC2 with GGplot2
+#######################################################################
+p <- ggplot(x, aes(x=PC1, y=PC2)) +
+  geom_text(label=rownames(x)) +
+  labs(
+    x=paste0("PC1 - ", pca_variance_percentage[1], "%"),
+    y=paste0("PC2 - ", pca_variance_percentage[2], "%")
+  )
 
 ggsave(
   filename="PCA_Plot.jpg",
@@ -115,20 +145,33 @@ ggsave(
   path=output
 )
 
-# Get center
-center <- as.data.frame(pca_info$center, stringsAsFactors = FALSE)
-center <- tibble::rownames_to_column(center, var = "Trait")
 
-# Get scale
-scale <- as.data.frame(pca_info$scale, stringsAsFactors = FALSE)
-scale <- tibble::rownames_to_column(scale, var = "Trait")
+#######################################################################
+## Plot PC1 vs PC2 with AutoPlot
+#######################################################################
+p <- autoplot(pca, data=pca_df, label=TRUE)
+
+ggsave(
+  filename="PCA_Autolot.jpg",
+  plot=p,
+  path=output
+)
+
+
+#######################################################################
+## Get all PCA information
+#######################################################################
+sdev <- pca$sdev %>% as.data.frame(stringsAsFactors = FALSE)
+rotation <- pca$rotation %>% as.data.frame(stringsAsFactors = FALSE)
+center <- pca$center %>% as.data.frame(stringsAsFactors = FALSE)
+scale <- pca$scale %>% as.data.frame(stringsAsFactors = FALSE)
 
 # Get PC
-PC <- data.frame(pc1 = pca_info$x[, 1], pc2 = pca_info$x[, 2], stringsAsFactors = FALSE)
+PC <- pca$x %>% as.data.frame(stringsAsFactors = FALSE)
 
-# Save all outputs to files
+# # Save all outputs to files
 write.csv(sdev, file.path(output, "sdev.csv"), row.names = FALSE)
-write.csv(rotation, file.path(output, "rotation.csv"), row.names = FALSE)
+write.csv(rotation, file.path(output, "rotation.csv"), row.names = TRUE)
 write.csv(center, file.path(output, "center.csv"), row.names = FALSE)
 write.csv(scale, file.path(output, "scale.csv"), row.names = FALSE)
-write.csv(PC, file.path(output, "PC.csv"), row.names = FALSE)
+write.csv(PC, file.path(output, "PC.csv"), row.names = TRUE)
